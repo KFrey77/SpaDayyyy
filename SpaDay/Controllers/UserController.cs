d60fcb240d72642d32bf768a18307a5bdd0aa579
Zombie <kroshel77@gmail.com>
2022-10-19 20:32:52 -0500

    Spa Dayyyy!

--- SpaDay/Controllers/UserController.cs
+++ SpaDay/Controllers/UserController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SpaDay.Models;
+using SpaDay.View_Model;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -19,26 +22,45 @@ namespace SpaDay.Controllers
 
         public IActionResult Add()
         {
-            return View();
+            
+            AddUserViewModel addUserViewModel = new AddUserViewModel();
+            
+                return View(addUserViewModel);
         }
 
         [HttpPost]
         [Route("/user")]
-        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        public IActionResult SubmitAddUserForm(AddUserViewModel addUserViewModel)
         {
-            if (newUser.Password == verify)
-            {
-                ViewBag.user = newUser;
-                return View("Index");
-            }
+            if (ModelState.IsValid)
+            { 
+                if (addUserViewModel.Password == addUserViewModel.VerifyPassword)
+                {
+
+                 User user = new User
+                    
+                {
+                    Username = addUserViewModel.Username,
+                    Password = addUserViewModel.Password,
+                    Email = addUserViewModel.Email,
+                    VerifyPassword = addUserViewModel.VerifyPassword
+                };
+
+                    return View("Index", user);
+                    }
+            
             else
             {
-                ViewBag.error = "Passwords do not match! Try again!";
-                ViewBag.userName = newUser.Username;
-                ViewBag.eMail = newUser.Email;
-                return View("Add");
-            }
+                    ViewBag.error = "Passwords do not match! Try again!";
+                    return View("Add", addUserViewModel);
+
+             }
+
+
         }
+                    return View("Add", addUserViewModel);
+
+    }
 
     }
 }
---
+++ SpaDay/View Model/AddUserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpaDay.View_Model
+{
+    public class AddUserViewModel
+    {
+        [Required]
+
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Username must be 5 to 15 characters.")]
+        public string Username { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Password must be 6 to 20 characters.")]
+        public string Password { get; set; }
+        [Required]
+        public string VerifyPassword { get; set; }
+    }
+
+    
+}
