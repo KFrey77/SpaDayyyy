37e8cbfe96ab00b279f0d9199d4281507a2e2835
Zombie <kroshel77@gmail.com>
2022-10-19 19:34:08 -0500

    Commit All

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Diagnostics;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route ("/user/add")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            if (newUser.Password == verify)
+            {
+                ViewBag.user = newUser.Username;
+                return View("Index");
+            }
+            else
+            {
+                return View("Add");
+            }
+            
+
+           /* EventData.Add(newEvent);
+
+            return Redirect("/Events");*/
+        }
+
+    }
+    //two parameters
+}
