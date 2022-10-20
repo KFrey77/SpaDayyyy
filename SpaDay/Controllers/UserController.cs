2141a9487040c7056c58cb50582941ec206399c7
Zombie <kroshel77@gmail.com>
2022-10-19 20:45:40 -0500

    View Models, Woo Models!

--- SpaDay/Controllers/UserController.cs
+++ SpaDay/Controllers/UserController.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity;*/
 using Microsoft.AspNetCore.Mvc;
 using SpaDay.Models;
 using SpaDay.View_Model;
